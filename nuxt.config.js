9a840ae798fab894c0cac4e13f36960e4fc8ee0b
Rionn Kenshin Pineda <rionnken@outlook.com>
2021-09-22 14:37:21 +0800

    Add amplify

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/cookies.js'
+    '~/plugins/cookies.js',
+    { src: '~/plugins/amplify.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
