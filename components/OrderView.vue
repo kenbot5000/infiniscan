162fe186e19386de9594e2515e3a07bd77851288
Rionn Kenshin Pineda <rionnken@outlook.com>
2021-11-07 14:54:31 +0800

    Order view on dashboard

--- components/OrderView.vue
+++ components/OrderView.vue
@@ -9,24 +9,55 @@
       </v-btn>
     </div>
     <div>
-      <v-list disabled>
-        <h5 class="text-h6">
-          Items
-        </h5>
-        <v-list-item v-for="item in items" :key="item.id">
+      <v-data-iterator :items="items" hide-default-footer class="my-5">
+        <template #header>
+          <v-row>
+            <v-col cols="8" class="subtitle-2">
+              Item Name
+            </v-col>
+            <v-col cols="2" class="subtitle-2">
+              Price
+            </v-col>
+            <v-col cols="2" class="subtitle-2">
+              Quantity
+            </v-col>
+          </v-row>
+        </template>
+        <template #default="props">
+          <v-row v-for="item in props.items" :key="item.name">
+            <v-col cols="8">
+              {{ item.name }}
+            </v-col>
+            <v-col cols="2">
+              <span>&#8369;{{ item.price }}</span>
+            </v-col>
+            <v-col cols="2">
+              <span>{{ item.quantity }}</span>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-iterator>
+
+      <!-- <v-list-item v-for="item in items" :key="item.id">
           <v-list-item-content>{{ item.name }}</v-list-item-content>
           <v-list-item-action>
-            <span>PHP {{ item.price }}</span>
+            <v-row>
+              <v-col>
+                <span>&#8369;{{ item.price }}</span>
+              </v-col>
+              <v-col>
+                <span>{{ item.quantity }}</span>
+              </v-col>
+            </v-row>
           </v-list-item-action>
-        </v-list-item>
-      </v-list>
+        </v-list-item> -->
       <v-card-actions class="mb-4">
         <v-spacer />
         <v-btn color="warning" class="darken-2" depressed>
-          Edit Order
+          Cancel Order
         </v-btn>
         <v-btn color="primary" depressed>
-          Resolve Order
+          Accept Order
         </v-btn>
       </v-card-actions>
     </div>
@@ -44,15 +75,26 @@ export default {
     testType: {
       type: Number,
       default: 0
+    },
+    content: {
+      type: Object,
+      default () {
+        return {};
+      }
     }
   },
   data () {
     return {
-      fullname: 'John Doe',
       items: []
     };
   },
+  computed: {
+    fullname () {
+      return `${this.content.user.firstname} ${this.content.user.lastname}`;
+    }
+  },
   mounted () {
+    // * Test items
     if (this.test) {
       switch (this.testType) {
         case 0:
@@ -73,6 +115,18 @@ export default {
           this.items = [
             { id: 2, name: 'Cheesecake Milk Tea', price: 85 }
           ];
+      }
+    }
+
+    // * Actual functionality
+    if (this.content.items) {
+      for (const item of this.content.items) {
+        if (this.items.some(o => o.name === item.name)) {
+          const index = this.items.map(e => e.name).indexOf(item.name);
+          this.items[index].quantity += 1;
+        } else {
+          this.items.push({ name: item.name, price: item.price, quantity: 1 });
+        }
       }
     }
   }
