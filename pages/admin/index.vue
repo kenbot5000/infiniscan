b26002e563df7c457977cbb7b3bd7d39c2a3167b
Rionn Kenshin Pineda <rionnken@outlook.com>
2021-11-10 01:57:51 +0800

    Adjust admin dialog

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -9,7 +9,27 @@
           </h3>
           <v-divider />
           <div class="d-flex flex-column">
-            <OrderView v-for="(item, i) of orders" :key="i" :content="item" class="order-view" />
+            <!-- Confirm Order Dialog -->
+            <v-dialog v-model="acceptOrderDialog" width="500">
+              <v-card>
+                <v-card-title>Accept Order?</v-card-title>
+                <v-divider />
+                <v-card-text class="mt-4">
+                  Are you sure you want to accept this order? This will consume stock from your inventory.
+                </v-card-text>
+                <v-divider />
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn color="error" @click="acceptOrderDialog = false">
+                    Close
+                  </v-btn>
+                  <v-btn color="primary" @click="confirmOrder">
+                    Confirm
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <OrderView v-for="(item, i) of orders" :key="i" :content="item" class="order-view" @show-accept="showAcceptOrderDialog" />
           </div>
         </v-card>
       </v-col>
@@ -39,7 +59,9 @@ export default {
   layout: 'admin',
   data () {
     return {
-      orders: []
+      orders: [],
+      acceptOrderDialog: false,
+      activeOrder: {}
     };
   },
   head () {
@@ -50,6 +72,12 @@ export default {
   async mounted () {
     const res = await this.$axios.get('/order/list?status=waiting');
     this.orders = res.data.res;
+  },
+  methods: {
+    showAcceptOrderDialog (order) {
+      this.acceptOrderDialog = true;
+      this.activeOrder = order;
+    }
   }
 };
 </script>
