b229a345f4f3b8b0c6e77a68fbcb5b0476226835
Rionn Kenshin Pineda <rionnken@outlook.com>
2021-11-30 17:28:06 +0800

    Add user management

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -5,11 +5,18 @@
         <v-card outlined class="pa-3">
           <v-row>
             <v-col>
-              <v-card-actions>
-                <h3 class="text-h3 ml-4 mt-2 mb-2">
-                  Customer Management
-                </h3>
-              </v-card-actions>
+              <h3 class="text-h3 ml-4 mt-2 mb-2">
+                Customer Management
+              </h3>
+              <v-data-table :headers="userHeaders" :items="items" :search="search">
+                <template #top>
+                  <v-text-field
+                    v-model="search"
+                    label="Search"
+                    class="mx-4"
+                  />
+                </template>
+              </v-data-table>
             </v-col>
           </v-row>
         </v-card>
@@ -21,7 +28,24 @@
 <script>
 export default {
   name: 'CustomerManagement',
-  layout: 'admin'
+  layout: 'admin',
+  data () {
+    return {
+      userHeaders: [
+        { text: 'ID', value: 'displayID', sortable: true },
+        { text: 'Email', value: 'email' },
+        { text: 'First Name', value: 'firstname' },
+        { text: 'Last Name', value: 'lastname' },
+        { text: 'Rewards Points', value: 'points' }
+      ],
+      items: [],
+      search: ''
+    };
+  },
+  async mounted () {
+    const { data } = await this.$axios.get('/user');
+    this.items = data.res;
+  }
 };
 </script>
 
