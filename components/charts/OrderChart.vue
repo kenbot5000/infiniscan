2f98968bc54ceb29fad1f9e078c5029358f4f6e5
Rionn Kenshin Pineda <rionnken@outlook.com>
2021-12-09 06:01:50 +0800

    Add PDF for report generation

--- components/charts/OrderChart.vue
+++ components/charts/OrderChart.vue
@@ -3,12 +3,20 @@ import { Bar } from 'vue-chartjs';
 
 export default {
   extends: Bar,
+  props: {
+    orderData: {
+      type: Array,
+      default () {
+        return [];
+      }
+    }
+  },
   data () {
     return {
       chartdata: {
         labels: ['test', 'tes', 'te', 't'],
         datasets: [{
-          label: 'Test Chart',
+          label: 'Recent Order Profits',
           backgroundColor: 'rgb(150, 255, 150)',
           data: [1, 2, 1, 4]
         }],
@@ -20,13 +28,21 @@ export default {
             ticks: {
               beginAtZero: true
             }
-
           }]
         }
       }
     };
   },
   mounted () {
+    const labels = [];
+    const data = [];
+    for (const order of this.orderData) {
+      labels.push(Object.keys(order)[0]);
+      data.push(Object.values(order)[0]);
+    }
+    this.chartdata.labels = labels;
+    this.chartdata.datasets[0].data = data;
+
     // Chart generation
     this.renderChart(this.chartdata, this.options);
   }
