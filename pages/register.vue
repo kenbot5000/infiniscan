1e3324e16a99025f152a3e1b48fc7b13166a4809
Rionn Kenshin Pineda <rionnken@outlook.com>
2022-04-22 09:30:01 +0800

    Add form validation

--- pages/register.vue
+++ pages/register.vue
@@ -10,16 +10,16 @@
             <v-alert v-if="alert.show" class="mt-4" dense border="left" type="warning">
               {{ alert.message }}
             </v-alert>
-            <v-text-field v-model="email" label="Email" />
-            <v-text-field v-model="password" label="Password" type="password" />
-            <v-text-field v-model="confirmPassword" label="Confirm Password" type="password" />
-            <v-text-field v-model="firstname" label="First Name" />
-            <v-text-field v-model="lastname" label="Last Name" />
-            <v-text-field v-model="phone" label="Phone" />
-            <v-text-field v-model="address1" label="Street Address" />
+            <v-text-field v-model="email" label="Email*" required />
+            <v-text-field v-model="password" label="Password*" type="password" required />
+            <v-text-field v-model="confirmPassword" label="Confirm Password*" type="password" required />
+            <v-text-field v-model="firstname" label="First Name*" required />
+            <v-text-field v-model="lastname" label="Last Name*" required />
+            <v-text-field v-model="phone" label="Phone*" required />
+            <v-text-field v-model="address1" label="Street Address*" required />
             <v-text-field v-model="address2" label="House/Block No." />
-            <v-text-field v-model="barangay" label="Barangay" />
-            <v-text-field v-model="city" label="City" />
+            <v-text-field v-model="barangay" label="Barangay*" required />
+            <v-text-field v-model="city" label="City*" required />
             <a href="#" @click="$router.push('/login')">Already have an account? Log in</a>
             <v-row class="mt-2">
               <v-col cols="3">
@@ -63,8 +63,20 @@ export default {
       this.alert.message = message;
     },
     async register () {
+      const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+      if (!(this.email === '' ? null : !!(reg.test(this.email)))) {
+        this.showAlert('Email must follow the format of email.com');
+        return;
+      }
+
+      if (!this.email || !this.password || !this.confirmPassword || !this.firstname || !this.lastname || !this.phone || !this.address1 || !this.barangay || !this.city) {
+        this.showAlert('Please fill out all required fields.');
+        return;
+      }
+
       if (this.password !== this.confirmPassword) {
         this.showAlert('The passwords must match.');
+        return;
       }
       const userData = {
         email: this.email,
